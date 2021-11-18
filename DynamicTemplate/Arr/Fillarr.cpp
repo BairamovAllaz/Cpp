5341085684172c6ba612c04ddd9586d29367730a
Bairamov Allaz <67458149+BairamovAllaz@users.noreply.github.com>
2021-11-18 23:56:20 +0300

    Delete DynamicTemplate/Arr directory

--- DynamicTemplate/Arr/Fillarr.cpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Fillarr.h"
-
-template <typename T1>
-void fillarray(T1 *arr, int *size, T1 ran)
-{
-    for (size_t i = 0; i < *size; i++)
-        arr[i] = rand() % 20 + ran;
-}
--- DynamicTemplate/Arr/Fillarr.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "stdafx.h"
-template <typename T1>
-void fillarray(T1 *arr, int *size, T1 ran);
