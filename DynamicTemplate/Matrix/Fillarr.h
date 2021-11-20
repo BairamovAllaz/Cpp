fa35681bd815705caf3e44f0cf7e84c59425d3eb
Bayramov Allaz <67458149+bayramovellez@users.noreply.github.com>
2021-11-21 01:29:20 +0300

    some

--- DynamicTemplate/Matrix/Fillarr.h
+++ DynamicTemplate/Matrix/Fillarr.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "stdafx.h"
-
 template <typename T1>
 void fill(T1 **matrix, int height, int width, T1 ran);
 template <typename T1>
 void fillarray(T1 *arr, int *size, T1 ran);
+//some
